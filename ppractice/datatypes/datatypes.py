9b3bc59f59497791f7ead02d642462e5d5ae1cab
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-05-02 23:08:42 +0530

    feat : completed python basics

---
+++ ppractice/datatypes/datatypes.py
@@ -0,0 +1,34 @@
+# data types
+# 1.int
+# 2.float
+# 3.string
+# 4.boolean
+# 5.char
+
+# a=10
+# print(type(a))
+# b=False
+# print(type(b))
+# c=0.000
+# print(type(c))
+# d='hello'
+# print(type(d))
+# e=5+3J
+# print(type(e))
+
+
+# 1.list []
+# 2.tuple ()
+# 3.set {}
+# 4.dictionary {}
+
+
+#type conversion
+# when data is lose -explicit
+# when dats is not lose-impilict
+# a=2
+# print(float(a))
+# print(type(a))
+# b=5.5
+# print(int(b))
+# print(type(b))
---
+++ ppractice/operators/operators.py
@@ -0,0 +1,61 @@
+# arithemtic operator
+# a=10
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+
+# COMPARISON OPERATOR
+# a=5
+# b=10
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+# LOGICAL OPERATORS
+# x=True
+# y=False
+# print(x and y)
+# print(x or y)
+# print(not x)
+
+
+# ASSIGNMENT OPERATOR
+# x=5
+# x+=3
+# x-=2
+# x*=2
+# x/=3
+# print(x)
+
+
+# BITWISE OPERATOR
+# a=5
+# b=3
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# print(~a)
+
+
+# MEMBERSHIP OPERATORS
+# nums=[1,2,3]
+# print(2 in nums)
+# print(5 not in nums)
+
+
+# IDENTITY OPERATOR
+# a=[1,2]
+# b=a
+# c=[1,2]
+# print(a is b)
+# print(a is c)
+# print(a is not c)
