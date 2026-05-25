c0ec5531d41c13fb0bd8f2109b087b599dbf107d
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-05-25 10:34:47 +0530

    chore : stage-2 completed upto packages/modules

---
+++ ppractice/stage-2/modules/module.py
@@ -0,0 +1,25 @@
+# import math_operations
+# print(math_operations.add(2,3))
+# print(math_operations.sub(5,6))
+
+
+#TYPES OF MODULES
+#1.BUILT IN MODULES(already availble in python)
+# import math
+# print(math.sqrt(25))
+
+#2.USER DEFINED MODULES
+# import math_operations
+# print(math_operations.add(2,3))
+# print(math_operations.sub(5,6))
+
+#3.different ways to import modules
+# import math
+# print(math.pi)
+
+#from math import sqrt
+#print(sqrt(25))
+
+# import math as m
+# print(m.sqrt(25))
+
---
+++ ppractice/stage-2/packages/main.py
@@ -0,0 +1,4 @@
+from add import addition
+from sub import sub
+print(addition(5,5))
+print(sub(5,6))
